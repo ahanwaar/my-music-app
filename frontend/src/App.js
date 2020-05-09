b2d77b0814fb43abf022d4e3a2fbf1c6c8706bce
Anwaar <anwaar95omar@gmail.com>
2020-05-09 04:09:59 +0300

    Testing components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import ArtistList from "./components/ArtistList";
 
 function App() {
   return (
     <div className="App">
         <h1>Artist Lists :</h1>
+        <ArtistList />
     </div>
   );
 }
---
+++ frontend/src/components/ArtistList.js
@@ -0,0 +1,9 @@
+import React  from "react";
+
+function ArtistList() {
+    return(
+        <h2>List</h2>
+    )
+}
+
+export default ArtistList;
