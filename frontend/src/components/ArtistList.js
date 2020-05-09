9e5df32a18097f3fe29c512c43d17493447a6e20
Anwaar <anwaar95omar@gmail.com>
2020-05-09 19:00:02 +0300

    Done

--- frontend/src/components/ArtistList.js
+++ frontend/src/components/ArtistList.js
@@ -1,11 +1,44 @@
 import React  from "react";
 
-function ArtistList(props){
-    return(
+function ArtistList(props) {
+    return (
         <div>
-            {props.artists.map( artist => {
-                return(<h4 key={artist.id} >{artist.name} </h4>); })}
-         </div>
+            {props.artists.map(artist => {
+                return (
+                    <ul key={artist.id}>
+                        <h3>{artist.name}</h3>
+                        <li>Albums:</li>
+                        <ul>
+                            {artist.albums.map(album => {
+                                return (
+                                    <ul>
+                                        <li>{album.title}</li>
+                                        <ul>
+                                            <li>{album.year}</li>
+
+                                            {album.songs.map(song => {
+                                                return (
+                                                    <ul>
+                                                        <li>{song.title}</li>
+                                                    </ul>
+                                                )
+                                            })}
+                                        </ul>
+
+                                    </ul>
+
+                                );
+                            })}
+
+                        </ul>
+                        <hr color="white"/>
+                    </ul>
+
+
+                );
+            })}
+        </div>
     )
 }
+
 export default ArtistList;
